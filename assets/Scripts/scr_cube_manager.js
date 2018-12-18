55bf9429e17c7513eb8e25d4331de0b8da2e0f57
ZoiElMapa <45901470+ZoiElMapa@users.noreply.github.com>
2018-12-18 18:57:31 -0500

    Variables required for the gameplay required

--- assets/Scripts/scr_cube_manager.js
+++ assets/Scripts/scr_cube_manager.js
@@ -2,6 +2,21 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+    	///Cube object
+    	spdboost: 1,
+    	hsp: 10.6,
+
+    	///Physics
+    	gravdir: 1,
+    	hsp: 0,
+    	vsp: 0,
+    	spd: 0.1,
+    	maxspd: 1,
+    	hdir: 1,
+    	grav: 1.5,
+    	avoidup: 0,
+    	maxvsp: 25,
+    	mini: 0
     },
 
     start () {
